24ed62a8d6384bed19024c9d425d50e360d04a0a
Tamsin Edwards <tamsin.edwards@kcl.ac.uk>
2025-08-05 13:47:08 +0100

    Go back to correct build file name in AIS run script after fixing temporary model_list bug

--- run_AIS.sh
+++ run_AIS.sh
@@ -35,11 +35,9 @@ outdir="$results_dir"/"$now"_AIS_ALL_"$final_year"
 
 echo
 echo run AIS: predict
-
- if [ "$final_year" -gt 2200 ]
+  if [ "$final_year" -gt 2200 ]
   then
-     build_file=AIS_ALL_Kori_PISM_CISM_ElmerIce_BISICLES_IMAUICE_pow_exp_10_EMULATOR.RData
-#     build_file=AIS_ALL_Kori_PISM_BISICLES_IMAUICE_pow_exp_10_EMULATOR.RData
+     build_file=AIS_ALL_Kori_PISM_BISICLES_IMAUICE_pow_exp_10_EMULATOR.RData
   fi
   if [ "$final_year" -le 2200 ]
   then
