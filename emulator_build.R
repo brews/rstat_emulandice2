f60915a9a51ad7e97876aa190499bc9cb1439493
Tamsin Edwards <tamsin.edwards@kcl.ac.uk>
2026-05-08 17:37:31 -0700

    Change GO model glacier completion thresholds
    For data quality filtering, use threshold of 85% glaciers completing for all regions, based on latest advice from Meg since fixing threshold values in dataset
    
    Only 90% of glaciers are run in GO, so this threshold corresponds to 94.4% of these.

--- emulator_build.R
+++ emulator_build.R
@@ -292,16 +292,10 @@ if (i_s == "GLA") {
   # Default completion thresholds
   complete_thresh <- list()
   complete_thresh[["OGGM"]] <- 0.95
-  complete_thresh[["GO"]] <- 0.80
+  complete_thresh[["GO"]] <- 0.85
 
   # Exceptions for OGGM
   if (reg_num %in% 16:18) complete_thresh[["OGGM"]] <- 0.80 # maybe keep 0.95 for 17?
-
-  # Exceptions for GO
-  if (reg_num %in% 3:4) complete_thresh[["GO"]] <- 0.70
-  if (reg_num == 13) complete_thresh[["GO"]] <- 0.50
-  if (reg_num %in% c(8,17)) complete_thresh[["GO"]] <- 0.40
-  if (reg_num == 14) complete_thresh[["GO"]] <- 0.20
 
 }
 
